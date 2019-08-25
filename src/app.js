24811dbe6533ba0b5365cdd70e2fb40571aa0baa
Adam Gajzlerowicz <nelf86@gmail.com>
2019-08-25 19:54:20 +0200

    added clock

--- src/app.js
+++ src/app.js
@@ -1,31 +1,12 @@
 import React, { PureComponent } from 'react'
-import { StyleSheet } from 'react-native'
-import Animated from 'react-native-reanimated'
+import { StyleSheet, View } from 'react-native'
 import { colors } from './themes'
 
-const { View, Value } = Animated
-
 export default class App extends PureComponent {
-  opacity = new Value(1)
-
-  constructor(props) {
-    super(props)
-    setTimeout(() => {
-      this.opacity.setValue(0.4)
-    }, 1000)
-  }
-
   render() {
     return (
       <View style={styles.container}>
-        <View
-          style={[
-            styles.box,
-            {
-              opacity: this.opacity
-            }
-          ]}
-        />
+        <View style={[styles.box]} />
       </View>
     )
   }
